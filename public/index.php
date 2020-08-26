1dca6f7b4c4c36c18f3f07afd2038d08320835d2
ludovicjj <jahanlud@gmail.com>
2020-08-26 18:30:12 +0200

    Init routeCollection and manage exception

--- public/index.php
+++ public/index.php
@@ -3,25 +3,43 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
-$pathInfo = $request->getPathInfo();
 $response = new Response();
 
-$map = [
-    '/hello' => __DIR__ . '/../src/pages/hello.php',
-    '/bye' => __DIR__ . '/../src/pages/bye.php'
-];
+$routes = new RouteCollection();
+$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
+$routes->add('bye', new Route('/bye'));
 
-if (isset($map[$pathInfo])) {
-    extract($request->query->all());
+$context = new RequestContext();
+$context->fromRequest($request);
+$urlMatcher = new UrlMatcher($routes, $context);
+
+$pathInfo = $request->getPathInfo();
+
+try {
+    extract($urlMatcher->match($pathInfo), EXTR_SKIP);
     ob_start();
-    include $map[$pathInfo];
+    /**
+     * @var $_route string
+     * @noinspection PhpIncludeInspection
+     */
+    include sprintf(__DIR__ . '/../src/pages/%s.php', $_route);
     $response->setContent(ob_get_clean());
-} else {
+} catch (ResourceNotFoundException $exception) {
     $response
-        ->setStatusCode(404)
-        ->setContent('Not found');
+        ->setContent('Page not found')
+        ->setStatusCode(404);
+} catch (Exception $exception) {
+    $response
+        ->setContent('Oops something is broken')
+        ->setStatusCode(500);
 }
+
 $response->send();
 
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,1 +1,6 @@
-Hello <?= htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
+Hello <?=
+/**
+ * @var $name string
+ */
+htmlspecialchars($name, ENT_QUOTES, 'UTF-8')
+?>
