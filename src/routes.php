4cc985937996b10e9802bcc6f5af89eb4308b762
ludovicjj <jahanlud@gmail.com>
2020-08-27 17:40:01 +0200

    Update route: associated controller

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,24 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 $routes = new RouteCollection();
-$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
-$routes->add('bye', new Route('/bye'));
+$routes->add(
+    'hello',
+    new Route(
+        '/hello/{name}',
+        [
+            'name' => 'World',
+            '_controller' => 'defaultController'
+        ]
+    )
+);
+$routes->add(
+    'bye',
+    new Route(
+        '/bye',
+        [
+            '_controller' => 'defaultController'
+        ]
+    )
+);
 
 return $routes;
