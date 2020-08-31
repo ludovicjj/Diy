bca2cd60bcdbd3a8d6a549e7c04f1c225c471042
ludovicjj <jahanlud@gmail.com>
2020-08-31 10:40:11 +0200

    Test: valid request

--- tests/FrameworkTest.php
+++ tests/FrameworkTest.php
@@ -4,10 +4,13 @@
 namespace Tests;
 
 
+use App\Controller\LeapYearController;
 use App\Core\Framework;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
@@ -15,23 +18,49 @@ use Symfony\Component\Routing\RequestContext;
 
 class FrameworkTest extends TestCase
 {
-    public function testNotFound()
+    public function testNotFound(): void
     {
-        $response = $this->initFrameworkError(new ResourceNotFoundException())->handle(new Request());
+        $response = $this->initFrameworkWithError(new ResourceNotFoundException())->handle(new Request());
         $this->assertEquals(404, $response->getStatusCode());
     }
 
-    public function testExceptionHandling()
+    public function testExceptionHandling(): void
     {
-        $response = $this->initFrameworkError(new \Exception())->handle(new Request());
+        $response = $this->initFrameworkWithError(new \Exception())->handle(new Request());
         $this->assertEquals(500, $response->getStatusCode());
+    }
+
+    public function testResponse(): void
+    {
+        $urlMatcher = $this->createMock(UrlMatcherInterface::class);
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        $urlMatcher
+            ->expects($this->once())
+            ->method('getContext')
+            ->willReturn($this->createMock(RequestContext::class));
+
+        $urlMatcher
+            ->expects($this->once())
+            ->method('match')
+            ->willReturn([
+                "_route" => "/is-leap-year",
+                "year" => "2020",
+                "_controller" => [new LeapYearController(), 'index']
+            ]);
+
+        $framework = new Framework($urlMatcher, $controllerResolver, $argumentResolver);
+        $response = $framework->handle(new Request());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('Yep, this is a leap year!', $response->getContent());
     }
 
     /**
      * @param mixed $exception
      * @return Framework
      */
-    private function initFrameworkError($exception)
+    private function initFrameworkWithError($exception)
     {
         $urlMatcher = $this->createMock(UrlMatcherInterface::class);
         $controllerResolver = $this->createMock(ControllerResolverInterface::class);
