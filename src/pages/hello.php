4082ee8158f291d155241374073d457961c1678e
ludovicjj <jahanlud@gmail.com>
2020-09-02 03:12:48 +0200

    ignore coverage

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,6 +1,6 @@
-Hello <?=
-/**
- * @var $name string
- */
-htmlspecialchars($name, ENT_QUOTES, 'UTF-8')
+Hello
+<?php
+if (isset($name)) {
+     echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+}
 ?>
