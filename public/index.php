563db2ce23a6902dc5009c91532dda5a156248a3
ludovicjj <jahanlud@gmail.com>
2020-09-09 13:55:56 +0200

    Update entry point with container builder

--- public/index.php
+++ public/index.php
@@ -1,18 +1,21 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Core\Framework;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouteCollection;
 
+/** @var RouteCollection $routes */
+$routes = include __DIR__ . '/../src/routes.php';
+/** @var ContainerBuilder $container */
+$container = include __DIR__ . '/../src/container.php';
 
 // Init request
 /** @var Request $request */
 $request = Request::createFromGlobals();
 
-// Get RouteCollection
-/** @var RouteCollection $routes */
-$routes = include __DIR__ . '/../src/routes.php';
+/** @var Response $response */
+$response = $container->get('framework')->handle($request);
+$response->send();
 
-$framework = new Framework($routes);
-$framework->handle($request)->send();
